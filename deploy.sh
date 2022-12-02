175848fb16f68ce94fd8008dbe8fd632cb8bb057
agruezo <agruezo@gmail.com>
2022-12-02 12:11:00 -0500

    update build file

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,8 @@ register_definition() {
 }
 
 update_service() {
-    if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != 
-        $revision ]]; then
-        echo "Error updating service."
+    if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+    echo "Error updating service."
     return 1
     fi
 }
